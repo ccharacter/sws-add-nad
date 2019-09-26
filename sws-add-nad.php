7b8348350ad9c6c35247b8ddec86ea6cde549777
Sharon Stromberg <sharon@ccharacter.com>
2019-09-26 17:57:37 -0400

    try new functions

--- sws-add-nad.php
+++ sws-add-nad.php
@@ -33,19 +33,57 @@ register_activation_hook( __FILE__, 'sws_add_nad_data' );
 
 class AddNAD
 {
+	private $typeArr=array(
+		"nad-conf"->"NAD Conf",
+		"nad-conf-opt"=>"NAD Conf/Code",
+		"nad-conf-oth"=>"NAD Conf+Oth",
+		"nad-conf-oth-opt"=>"NAD Conf/Code+Oth",
+		"nad-union"=>"NAD Union",
+		"nad-union-opt"=>"NAD Union/Code",
+		"nad-union-oth"=>"NAD Union+Oth",
+		"nad-union-oth-opt"=>"NAD Union/Code+Oth",
+		"nad-all"=>"NAD Unions & Confs",
+		"nad-all-opt"=>"NAD Unions & Confs/Code",
+		"nad-all-oth"=>"NAD Unions & Confs+Oth",
+		"nad-all-oth-opt"=>"NAD Unions & Confs/Code +Oth");
 	
-	public function addGF() {
+	public function addGFCustom($class,$title) {
 		$old=get_option('gform_custom_choices');
 		$data = maybe_unserialize($old); 
-		//update_option('my_test',$data);
-		if (!(array_key_exists('MY TITLE'))) {
-			$data['MY TITLE']=array('One | 1','Two | 2','Three | 3');
+		if (!(array_key_exists($title,$data))) {
+			$data[$title]=getGFOpts(explode("-",$class));
 		
-			error_log(print_r($data,true),0);
+			//error_log(print_r($data,true),0);
 		
-			update_option('gform_custom_choices',$data);
+			//update_option('gform_custom_choices',$data);
 		}
-		//update_option('add_nad_test','I DID THIS AGAIN!');
+	}
+	
+	public function getGFOpts($arr) {
+		global $wpdb;
+		$table_name = $wpdb->prefix . 'sws_add_nad';
+		
+		switch($arr[1]) {
+				case("conf"): $cond="where `u_tag`='N'"; break;
+				case("union"): $cond="where `u_tag`='Y'"; break;
+				default: $cond=""; break;
+		}
+		if (($arr[2]=="opt") || ($arr[3]=="opt")) { $col="concat(`full_text`,'|',`id`)"; } 
+			else { $col="`full_text`";}
+			
+		$sql="select $col from $table_name $cond order by `full_text`";
+		$results = $wpdb->get_results($sql);
+		
+		error_log(print_r($results,true),0);
+		
+		return $results;
+		
+	}
+	
+	public function addGF() {
+		foreach ($typeArr as $class=>$title) { 
+			addGFCustom($class,$title);
+		}
 	}
 
 	public function addACF() {
